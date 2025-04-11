eef134f0a2f894a36d2a85b686dddb801cd9c032
Kanivin <info.kanivin@gmail.com>
2025-04-11 16:34:42 +0530

    cms removed
    cms removed

--- kanierp/crm/doctype/appointment_booking_settings/test_appointment_booking_settings.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Kanivin Pvt. Ltd. and Contributors
-# See license.txt
-
-# import frappe
-import unittest
-
-
-class TestAppointmentBookingSettings(unittest.TestCase):
-	pass
--- kanierp/crm/doctype/contract_template/test_contract_template.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2025, Kanivin Pvt. Ltd. and Contributors
-# See license.txt
-
-import unittest
-
-
-class TestContractTemplate(unittest.TestCase):
-	pass
