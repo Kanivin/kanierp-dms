9041ef417cf5e323a9ea675ec1cda416a3aac4b6
Kanivin <info.kanivin@gmail.com>
2025-04-11 17:49:58 +0530

    kanierp integrations
    kanierp integrations

--- kanierp/public/js/conf.js
+++ kanierp/public/js/conf.js
@@ -17,7 +17,7 @@ $.extend(frappe.breadcrumbs.preferred, {
 });
 
 $.extend(frappe.breadcrumbs.module_map, {
-	"Kanivin Integrations": "Integrations",
+	"kanierp integrations": "Integrations",
 	Geo: "Settings",
 	Portal: "Website",
 	Utilities: "Settings",
