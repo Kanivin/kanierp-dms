f54b0fcaee24a5d26a1b9748f2b7945be5e35aff
Kanivin <info.kanivin@gmail.com>
2025-04-11 17:07:21 +0530

    cmd
    cmd

---
+++ kanierp/crm/doctype/contract_fulfilment_checklist/contract_fulfilment_checklist.js
@@ -0,0 +1,6 @@
+// Copyright (c) 2025, Kanivin Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Contract Fulfilment Checklist", {
+	refresh: function (frm) {},
+});
